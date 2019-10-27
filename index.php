f6a28510cd56af430eb97b44f0787e0860e8f494
Rafa <wirecobweb@gmail.com>
2019-10-27 14:57:07 +0100

    problem in conexion db

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 
 use DBControlador\CrearTablas;
 use DBControlador\oConexion;
+$config = parse_ini_file(__DIR__ . "/.env");
+
+
 
 spl_autoload_register(function($className){
+
     if(strpos($className, 'DBControlador\\')===0) {
         $className = str_replace('DBControlador\\', '', $className);
         if (file_exists("src/$className.php")) {
@@ -12,6 +16,5 @@ spl_autoload_register(function($className){
     }
 });
 
-
-$oConexion = new oConexion("localhost", "COUNTRIESDATA", "root", "");
+$oConexion = new oConexion($config["HOST"], $config["DB"], $config["USER"], $config["PASS"]);
 $crearTabla = new CrearTablas($oConexion);
